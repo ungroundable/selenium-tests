cda183fb7568b162f6d35615c0c9424f2e07d767
Andrey Dmitriev <a.dmitriev86@gmail.com>
2016-12-09 16:55:39 +0300

    tesk11 complite

--- java-example/src/test/java/ru/stqa/training/selenium/Test11.java
+++ java-example/src/test/java/ru/stqa/training/selenium/Test11.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
-
+import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
 
 /**
  * Created by Андрей on 07.12.2016.
@@ -16,14 +16,22 @@ public class Test11 extends TestBase {
 
     @Test
     public void NewUserRegister() {
+        long uniqueLong = System.currentTimeMillis() / 1000L;
+        String password = "password";
+        String newUserEmail = userRegister(uniqueLong, password);
+        logoutUser();
+        loginUser(newUserEmail,password);
+        logoutUser();
+    }
 
+    public String userRegister(long uniqueLong, String password){
         driver.navigate().to("http://localhost/litecart/en/");
 
         WebElement registerNewUserElement = driver.findElement(By.xpath("//*[contains(text(),'New customers click here')]"));
         registerNewUserElement.click();
 
         //use system time to generate unique values
-        long uniqueLong = System.currentTimeMillis() / 1000L;
+
 
         //user first name
         String newFirstName ="Name" + uniqueLong;
@@ -58,8 +66,8 @@ public class Test11 extends TestBase {
 
         //Zone
         String zone ="California";
-        WebElement zoneElement = wait.until(presenceOfElementLocated(By.cssSelector("select[name='zone_code']")));
-//        WebElement zoneElement = driver.findElement(By.cssSelector("select[name='zone_code']"));
+        wait.until(textToBePresentInElementLocated(By.cssSelector("select[name='zone_code']"),"Alabama"));
+        WebElement zoneElement = driver.findElement(By.cssSelector("select[name='zone_code']"));
         Select zoneSelect = new Select(zoneElement);
         zoneSelect.selectByVisibleText(zone);
 
@@ -74,7 +82,7 @@ public class Test11 extends TestBase {
         phoneElement.sendKeys(phone);
 
         //Password, not necessary to be unique
-        String password ="password";
+//        String password ="password" have one as a parameter;
         WebElement passwordElement = driver.findElement(By.cssSelector("*[name='password']"));
         passwordElement.sendKeys(password);
 
@@ -88,10 +96,31 @@ public class Test11 extends TestBase {
         WebElement createAccountElement = driver.findElement(By.cssSelector("*[name='create_account']"));
         createAccountElement.click();
 
-//        cityElement.sendKeys(city);
+        return email;
+    }
+
+    public void logoutUser(){
+        //wait until new page will load
+        wait.until(presenceOfElementLocated(By.cssSelector("#box-account li:last-child")));
+        WebElement logoutElement = driver.findElement(By.cssSelector("#box-account li:last-child a"));
+        logoutElement.click();
 
     }
 
+    public void loginUser(String newUserEmail,String password){
+        //wait until new page will load
+        wait.until(presenceOfElementLocated(By.cssSelector("*[name='email']")));
+        WebElement emailElement = driver.findElement(By.cssSelector("*[name='email']"));
+        emailElement.sendKeys(newUserEmail);
+
+        WebElement passwordElement = driver.findElement(By.cssSelector("*[name='password']"));
+        passwordElement.sendKeys(password);
+
+        WebElement loginElement = driver.findElement(By.cssSelector("*[name='login']"));
+        loginElement.click();
+
+
+    }
 
 
 
